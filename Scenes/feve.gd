92800108e51b6b1070a39228ff6ffd41d186a4fa
Marinee Samson <henri.journel@gmail.com>
2026-03-17 15:16:57 +0100

    add fev exploson when hiting enemy, created charizard sprite

--- Scenes/feve.gd
+++ Scenes/feve.gd
@@ -1,3 +1,4 @@
+class_name Feve
 extends AnimatedSprite2D
 
 @export var explosion:AnimatedSprite2D
@@ -8,18 +9,21 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func _playAnimation():
 	play("default")
 
 func _playKill():
-	explosion.play("default")
+	play("default")
+	#TODO add hurting to life bar and tell the enemysprite to be hurt
+	pass
+
 
 func _delete():
 	queue_free()
 	
 func _on_timer_timeout():
-	$AnimationPlayer.play("explode")
+	#$AnimationPlayer.play("explode")
 	pass # Replace with function body.
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var can_move = false
 const SPEED = 600.0
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	
 	var direction = Input.get_vector("left","right","up","down")
 	velocity= direction * SPEED
