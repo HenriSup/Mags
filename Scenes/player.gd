a0e8dbf575d1669136f207be13f84444a4ccfd24
Marinee Samson <henri.journel@gmail.com>
2026-03-19 03:06:36 +0100

    add sfx

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var player_bullet: PackedScene
+@export var shooting_sprite: AnimatedSprite2D
 var can_move = false
 const SPEED = 600.0
 
@@ -9,6 +11,20 @@ func _physics_process(_delta):
 	velocity= direction * SPEED
 	if can_move:
 		move_and_slide()
+		
+	var wantsToShoot = false
+	if Input.is_action_pressed("shoot"):
+		wantsToShoot = true
+	if can_move and wantsToShoot and $CanShootTimer.is_stopped() :
+		var newPlayerBullet = player_bullet.instantiate()
+		newPlayerBullet.global_position=$Offset/SpriteHolder/ShakeContainer/Marker2D.global_position
+		get_tree().root.add_child(newPlayerBullet)
+		$CanShootTimer.start()
+		shooting_sprite.frame=0
+		shooting_sprite.play("default")
+		$ShootSound.play()
+		pass
+
 
 func _allow_movement():
 	can_move=true
